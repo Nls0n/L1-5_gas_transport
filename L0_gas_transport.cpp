8f5bbae0e8a1d0c68cc859f3c7615c8bff33b320
Nls0n <arseny.2006.myrz@mail.ru>
2025-09-09 01:24:17 +0300

    realize all classes

--- L0_gas_transport.cpp
+++ L0_gas_transport.cpp
@@ -5,21 +5,32 @@
 
 using namespace std;
 
+bool is_float(string str) {
+    regex float_regex(R"(^[-+]?\d*\.?\d+$)"); // при надобности могу объяснить что значит эта регулярка
+    if (regex_match(str, float_regex)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool is_int(string str) {
+    regex int_regex(R"(^[-+]?\d+)");
+    if (regex_match(str, int_regex)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 class Pipe {
     public:
         string km_mark;
         float length; // km
         float diameter; // mm
         bool is_in_repair;
-        bool is_float(string str) {
-            regex float_regex(R"(^[-+]?\d*\.?\d+$)"); // при надобности могу объяснить что значит эта регулярка
-            if (regex_match(str, float_regex)) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
         void read_from_console() {
             string input;
             cout << "Input kilometers mark (name)" << endl;
@@ -66,14 +77,10 @@ class Pipe {
             }
         }
         void write_to_console() {
-            if (length && diameter && is_in_repair) {
-                cout << "Pipe length (km) " << length << endl;
-                cout << "Pipe diameter (mm) " << diameter << endl;
-                cout << "Pipe repair status " << is_in_repair << endl;
-            }
-            else {
-                cout << "Data is not given" << endl;
-            }
+            cout << "Pipe km_mark " << km_mark << endl;
+            cout << "Pipe length (km) " << length << endl;
+            cout << "Pipe diameter (mm) " << diameter << endl;
+            cout << "Pipe is_in_repair " << is_in_repair << endl;
         }
         void change_status() {
             is_in_repair = !is_in_repair;
@@ -94,12 +101,14 @@ class Pipe {
         }
         void load_from_file(string filename) {
             string line;
-            string word = "";
-            string value = "";
+            string word;
+            string value;
             int whitespace_counter = 0;
             ifstream in(filename);
             if (in.is_open()) {
                 while (getline(in, line)) {
+                    word = "";
+                    value = "";
                     for (int i = 0; i < line.length(); i++) {
                         if (whitespace_counter > 1) {
                             cout << "Incorrect data format" << endl;
@@ -169,17 +178,171 @@ class Pipe {
 };  
 
 class CompressorStation {
-    public:
-        string name;
-        int workshop_count;
-        int current_working_workshop_count; 
-        float station_cls; // я так и не понял какой нужен тип
+public:
+    string name;
+    int workshop_count;
+    int current_working_workshop_count;
+    float station_cls; // я так и не понял какой нужен тип
+    void read_from_console() {
+        string input;
+        cout << "Input CS name" << endl;
+        cin >> name;
+        cout << "name " << name << endl;
+        cout << "Input workshop_count" << endl;
+        cin >> input;
+        if (is_int(input)) {
+            int temp;
+            temp = stoi(input);
+            if (temp < 0) {
+                cout << "workshop_count must be positive" << endl;
+            }
+            else {
+                workshop_count = temp;
+                cout << "workshop_count " << workshop_count << endl;
+            }
+        }
+        cout << "Input current_working_workshop_count" << endl;
+        cin >> input;
+        if (is_int(input)) {
+            int temp;
+            temp = stoi(input);
+            if (temp < 0) {
+                cout << "current_working_workshop_count must be positive" << endl;
+            }
+            else {
+                current_working_workshop_count = temp;
+                cout << "current_working_workshop_count " << current_working_workshop_count << endl;
+            }
+        }
+        cout << "Input station_cls" << endl;
+        cin >> input;
+        if (is_float(input)) {
+            float temp;
+            temp = stof(input);
+            if (temp < 0) {
+                cout << "station_cls must be positive" << endl;
+            }
+            else {
+                cout << "station_cls " << station_cls << endl;
+            }
+        }
+        else {
+            cout << "Incorrect data format" << endl;
+        }
+    }
+    void write_to_console() {
+        cout << "CS name " << name << endl;
+        cout << "CS workshop_count " << workshop_count << endl;
+        cout << "CS current_working_workshop_count " << current_working_workshop_count << endl;
+        cout << "CS station_cls " << station_cls << endl;
+    }
+    void start_workshop() {
+        current_working_workshop_count++;
+    }
+    void stop_workshop() {
+        if (current_working_workshop_count > 0) {
+            current_working_workshop_count--;
+        }
+        else {
+            cout << "There are no avialiable workshops " << endl;
+        }
+    }
+    void save_to_file() {
+        ofstream out;
+        out.open("CS.txt");
+        if (out.is_open())
+        {
+            out << "name " << name << endl;
+            out << "workshop_count" << workshop_count << endl;
+            out << "current_working_workshop_count" << current_working_workshop_count << endl;
+            out << "station_cls" << station_cls << endl;
+        }
+        out.close();
+        cout << "data saved to file" << endl;
+    }
+    void load_from_file(string filename) {
+        string line;
+        string word;
+        string value;
+        int whitespace_counter = 0;
+        ifstream in(filename);
+        if (in.is_open()) {
+            while (getline(in, line)) {
+                word = "";
+                value = "";
+                for (int i = 0; i < line.length(); i++) {
+                    if (whitespace_counter > 1) {
+                        cout << "Incorrect data format" << endl;
+                        break;
+                    }
+                    if (line[i] && whitespace_counter == 0) {
+                        word += line[i];
+                    }
+                    else if (!line[i]) {
+                        whitespace_counter++;
+                    }
+                    if (line[i] == '\n') {
+                        if (word == "name") {
+                            name = value;
+                        }
+                        else if (word == "workshop_count") {
+                            if (is_int(value)) {
+                                int temp;
+                                temp = stoi(value); // string -> int
+                                if (temp < 0) {
+                                    cout << "workshop_count must be positive" << endl;
+                                }
+                                else {
+                                    workshop_count = temp;
+                                }
+                            }
+                            else {
+                                cout << "Incorrect format of workshop_count" << endl;
+                            }
 
+                        }
+                        else if (word == "current_working_workshop_count") {
+                            if (is_float(value)) {
+                                int temp;
+                                temp = stoi(value); // string -> int
+                                if (temp < 0) {
+                                    cout << "current_working_workshop_count must be positive" << endl;
+                                }
+                                else {
+                                    current_working_workshop_count = temp;
+                                }
+                            }
+                            else {
+                                cout << "Incorrect format of current_working_workshop_count" << endl;
+                            }
+                        }
+                        else if (word == "station_cls") {
+                            if (is_float(value)) {
+                                station_cls = stof(value);
+                                cout << "station_cls " << station_cls << endl;
+                            }
+                            else {
+                                cout << "Incorrect format of station_cls";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            cout << "File is not in working directory" << endl;
+        }
+        in.close();
+    }
 };
+
+
 
 int main()
 {
     int user_input;
+    Pipe pipe;
+    CompressorStation cs;
     while (true) {
         cin >> user_input;
         switch (user_input){
@@ -196,4 +359,3 @@ int main()
     }
     return 0;
 }
-
